8aa81c45ce2a7be547ac33f58ed8120ef5f427c8
Danilson Sanches <dsanches06@outlook.com>
2020-10-27 23:53:24 +0000

    testar a API de Books, foi usado o @SpringBootTest para carregar o contexto do aplicativo e verificar se não há erros ao executar o aplicativo

--- src/test/java/com/danilson/appspringboot/AppspringbootApplicationTests.java
+++ src/test/java/com/danilson/appspringboot/AppspringbootApplicationTests.java
@@ -1,13 +1,16 @@
 package com.danilson.appspringboot;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class AppspringbootApplicationTests {
+public class AppspringbootApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
